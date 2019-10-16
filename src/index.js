11e61bb16849709bb96606bc4380bedcf75e83f6
Jessica Munoz <ms.jessica.munoz@gmail.com>
2019-10-15 18:35:55 -0700

    It will have basic project structure

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
+  return (
+    <div>
+      Here we go just one more time and everybody's feeling fine
+      <br />
+      Here we go now, here we go, yeah
+      <br />
+      If you want to party with us, just feel free and feel the rhythm
+      <br />
+      Here we go now, here we go
+    </div>
+  );
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.querySelector("#root"));
