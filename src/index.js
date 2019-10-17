6b6c13128dbff24ea4beeee12967f45f2eda998e
Jessica Munoz <ms.jessica.munoz@gmail.com>
2019-10-17 06:54:32 -0700

    It will have geoloaction and SeasonDisplay component

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-  return (
-    <div>
-      Here we go just one more time and everybody's feeling fine
-      <br />
-      Here we go now, here we go, yeah
-      <br />
-      If you want to party with us, just feel free and feel the rhythm
-      <br />
-      Here we go now, here we go
-    </div>
-  );
-};
+
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => console.log(position),
+      err => console.log(err)
+    );
+    return <div>Latitude: </div>;
+  }
+}
+
 
 ReactDOM.render(<App />, document.querySelector("#root"));
