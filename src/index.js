93addcff809a94f06b209e084e0b2ed031741505
Jessica Munoz <ms.jessica.munoz@gmail.com>
2019-10-18 19:23:32 -0700

    It will have state for latitude geolocation

--- src/index.js
+++ src/index.js
@@ -3,12 +3,23 @@ import ReactDOM from "react-dom";
 
 
 class App extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: null};
+
     window.navigator.geolocation.getCurrentPosition(
-      position => console.log(position),
+      position => {
+        this.setState({lat: position.coords.latitude })
+      },
       err => console.log(err)
     );
-    return <div>Latitude: </div>;
+  }
+
+
+
+  render() {
+    return <div>Latitude: {this.state.lat} </div>;
   }
 }
 
